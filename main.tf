07faba18a49c9e8854b8b6926febbbe2b39b0dd4
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-20 10:53:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,39 +41,22 @@ resource "azurerm_linux_virtual_machine" "BoogeymanVirtualLinuxMachine" {
   name                = "Boogeyman-machine"
   resource_group_name = azurerm_resource_group.resource_gp1.name
   location            = azurerm_resource_group.resource_gp1.location
-  vm_size               = "Standard_DS1_v2"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    Owner = "Nick Colyer"
   }
 }
