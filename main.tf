126128194e600bc658a054ae2fe070aa4fbb4c4a
Boogeyman97-ops <67091478+Boogeyman97-ops@users.noreply.github.com>
2020-08-20 08:45:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "azurerm_linux_virtual_machine" "BoogeymanVirtualLinux" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("id_rsa.pub")
   }
 
   os_disk {
